2767c6c3c49abd411691b038c848eab5b632f058
Brian Kim <brianhockey98@gmail.com>
2019-11-03 19:47:42 -0300

    hay un problema en el insertCommand, despues lo demas supongo que andaria

--- FrbaOfertas2/FrbaOfertas2/Form1.cs
+++ FrbaOfertas2/FrbaOfertas2/Form1.cs
@@ -14,7 +14,9 @@ namespace FrbaOfertas2
     public partial class AltaRol_Form : Form
     {
 
+        SqlDataAdapter generico = new SqlDataAdapter();
         private DataTable tabla_funcionalidades = new DataTable();
+        private List<int> func_codigo_aux = new List<int>();
 
 
         public AltaRol_Form()
@@ -38,7 +40,7 @@ namespace FrbaOfertas2
         {
             SqlConnection connection = ConnectionWithDatabase();
 
-            String query_obtenerFuncionalidades = "SELECT func_nombre FROM S_QUERY.Funcionalidad";
+            String query_obtenerFuncionalidades = "SELECT func_codigo, func_nombre FROM S_QUERY.Funcionalidad";
             SqlDataAdapter sda = new SqlDataAdapter(query_obtenerFuncionalidades, connection);
 
             try
@@ -65,6 +67,8 @@ namespace FrbaOfertas2
         {
             listBox_funcionalidades_para_rol.Items.Add(comboBox_funcionalidades.SelectedValue);
 
+            func_codigo_aux.Add(tabla_funcionalidades.Rows[comboBox_funcionalidades.SelectedIndex].Field<int>(0));
+
             tabla_funcionalidades.Rows.RemoveAt(comboBox_funcionalidades.SelectedIndex);
             comboBox_funcionalidades.DataSource = tabla_funcionalidades;
         }
@@ -72,8 +76,37 @@ namespace FrbaOfertas2
         private void button_crear_rol_Click(object sender, EventArgs e)
         {
             SqlConnection connection = ConnectionWithDatabase();
-            String insert_rol_string = "INSERT INTO S_QUERY.Rol(rol_nombre, rol_estado) VALUES(textBox_nombre.Text, 1)";
-            String insert_rol_x_funcionalidad_string = "INSERT INTO S_QUERY.FuncionalidadxRol";
+            connection.Open();
+            //NO ANDA, INTERNET MENTIROSO
+            //String query_insert_rol = "INSERT INTO S_QUERY.Rol(rol_nombre, rol_estado) VALUES(textBox_nombre.Text, 1)";
+            //voy a probar con esto
+            String query_insert_rol_nuevo = "INSERT INTO S_QUERY.Rol(rol_nombre, rol_estado) VALUES('" + textBox_nombre.Text.ToString() + "', 1)";
+            //SqlDataAdapter sda_insert = new SqlDataAdapter(query_insert_rol, connection);
+            generico.InsertCommand = new SqlCommand(query_insert_rol_nuevo, connection);
+            generico.InsertCommand.ExecuteNonQuery();
+
+
+            String query_select_rol = "SELECT rol_codigo FROM S_QUERY.Rol WHERE rol_nombre = '" + textBox_nombre.Text.ToString() + "'";
+            SqlDataAdapter sda_select = new SqlDataAdapter(query_select_rol, connection);
+            DataTable data_rol = new DataTable();
+
+            sda_select.Fill(data_rol);
+
+
+            for (int i = 0; i < listBox_funcionalidades_para_rol.Items.Count; i++)
+            {
+                String insert_rol_x_funcionalidad =
+                    "INSERT INTO S_QUERY.FuncionalidadxRol(func_codigo, rol_codigo) VALUES(" +
+                    func_codigo_aux[i] + ", " +
+                    data_rol.Rows[0] + ")";
+
+                generico.InsertCommand = new SqlCommand(insert_rol_x_funcionalidad, connection);
+                generico.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("Se inserto una fila");
+            }
+
+        connection.Close();
+            
         }
 
         private void listBox_funcionalidades_para_rol_SelectedIndexChanged(object sender, EventArgs e)
